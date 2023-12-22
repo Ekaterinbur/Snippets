9aa10809bf49caa520e97a401ba146c130247ddf
Ekaterinbur <kate-buravkova@mail.ru>
2023-12-22 22:29:14 +0300

    M5:change snippets form

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 
@@ -9,9 +10,32 @@ def index_page(request):
     return render(request, 'pages/index.html', context)
 
 
+# def add_snippet_page(request):
+#     context = {'pagename': 'Добавление нового сниппета'}
+#     return render(request, 'pages/add_snippet.html', context)
+
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+        'pagename': 'Добавление нового сниппета',
+        'form':form,       
+        }
+        return render(request, 'pages/add_snippet.html', {'form': form})
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list")
+        return render(request, 'add_snippet.html', {'form': form})
+
+def create_snippet(request):
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect("snippets-list")
+    return render(request,'add_snippet.html',{'form': form})
 
 
 def snippets_page(request):
